17925847570b4d09bc368c47eb86c71102d03833
Slavcheva <stoyka.slavcheva@gmail.com>
2024-07-21 15:41:01 +0300

    Add Employee Class

---
+++ src/Employee.java
@@ -0,0 +1,17 @@
+public class Employee {
+    private int id;
+    private String name;
+    private String department;
+    private String role;
+    private double salary;
+    private boolean isEmployed;
+
+    public Employee(int id, String name, String department, String role, double salary, boolean isFire) {
+        this.id = id;
+        this.name = name;
+        this.department = department;
+        this.role = role;
+        this.salary = salary;
+        this.isEmployed = true;
+    }
+}
